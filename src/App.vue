bf046b35e8a0da272617f31d0f784d075e5466db
Boluwatife2904 <sanvicola2000@gmail.com>
2021-07-16 21:05:31 +0100

    Added assets, added fonts and css reset stylings

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
+  <div class="container">
+    <div class="logo">
+      <img src="./assets/images/logo.svg" alt="Logo">
+    </div>
+    <div class="tip-calculator">
+
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap");
 
 * {
   margin: 0;
@@ -22,5 +30,33 @@ html {
 
 body {
   font-family: "Space Mono", sans-serif;
+}
+
+#app {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: hsl(185, 41%, 84%);
+
+  .container {
+    max-width: 800px;
+    margin: 0 auto;
+    width: 95%;
+
+    .logo {
+      img {
+        display: block;
+        margin: 0 auto 80px;
+      }
+    }
+
+    .tip-calculator {
+      background: hsl(0, 0%, 100%);
+      padding: 100px;
+      border-radius: 20px;
+    }
+  }
 }
 </style>
